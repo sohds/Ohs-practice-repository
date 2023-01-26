03e22a56b799861cdc3700b0efdaf9d2185f6776
Seoyeon Oh <sohtks@swu.ac.kr>
2023-01-27 00:20:02 +0900

    second commit

--- PGS/[PGS] 코딩테스트 입문 day18.py
+++ PGS/[PGS] 코딩테스트 입문 day18.py
@@ -32,3 +32,10 @@ def solution(n):
 # 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
 def solution(n, t):
     return n * (2 ** t)
+
+
+# 문자열 정렬하기(2) (+2)
+# 문제 설명 : 영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때, 
+# my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요.
+def solution(my_string):
+    return ''.join(sorted(list(my_string.lower())))
